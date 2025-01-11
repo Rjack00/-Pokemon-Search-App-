c83b8aec8af5605ea62bf4698e714c5cfb1359fe
Rjack00 <rjackson77@comcast.net>
2025-01-10 18:29:09 -0700

    script.js updated
    added searchButton listener and updtaded code blocks for fetchData() and showResults()

--- script.js
+++ script.js
@@ -2,19 +2,47 @@ const urlQuery = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 
+
 const fetchData = async () => {
+  const cleanSearchInput = searchInput.value.toLowerCase().replace(/[^a-z0-9]/g, '');
+  console.log(cleanSearchInput);
   try {
-    const res = await fetch(urlQuery);
+    const res = await fetch(`${urlQuery}/${cleanSearchInput}`);
     const data = await res.json();
     showResults(data);
   }catch (error) {
-    console.error(`The following error occured while fetching data from URL: https://pokeapi-proxy.freecodecamp.rocks/api/pokemon...
+    console.error(`The following error occurred while fetching data from URL: https://pokeapi-proxy.freecodecamp.rocks/api/pokemon...
 ERROR: ${error}`);
+    alert("Pokémon not found");
   }
 }
 
-fetchData();
+
 
 const showResults = (data) => {
+  const {
+    name, id, weight, height, sprites, types, stats
+  } = data;
+  const typeNames = types.map(({type}) => type.name);
+  const hp = stats[0].base_stat;
+  const attack = stats[1].base_stat;
+  const defense = stats[2].base_stat;
+  const specialAttack = stats[3].base_stat;
+  const specialDefense = stats[4].base_stat;    
+  const speed = stats[5].base_stat;
+  const image = sprites.front_default;
+
   console.log("Data: ", data);
+
+
 }
+
+searchButton.addEventListener('click', () => {
+  
+  if(searchInput.value) {
+    fetchData();
+    searchInput.value = '';
+  } else {
+    alert ("Please enter a Pokémon name or ID");
+  }
+})
