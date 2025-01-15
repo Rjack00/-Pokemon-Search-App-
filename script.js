6844a8ba02d829f989111961e7f6020b20a00255
Rjack00 <rjackson77@comcast.net>
2025-01-14 19:01:38 -0700

    script.js updated
    updated typeNames.map

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const urlQuery = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const pokemonWeight = document.getElementById("weight");
+const pokemonHeight = document.getElementById("height");
+const pokemonTypes = document.getElementById("types");
+const pokemonImage = document.getElementById("image");
+const pokemonHp = document.getElementById("hp");
+const pokemonAttack = document.getElementById("attack");
+const pokemonDefense = document.getElementById("defense");
+const pokemonSpAttack = document.getElementById("special-attack");
+const pokemonSpDefense = document.getElementById("special-defense");
+const pokemonSpeed = document.getElementById("speed");
 
 
 const fetchData = async () => {
@@ -34,10 +46,39 @@ const showResults = (data) => {
 
   console.log("Data: ", data);
 
+  pokemonName.textContent = `${name.toUpperCase()}`;
+  pokemonId.textContent = `#${id}`;
+  pokemonWeight.textContent = `Weight: ${weight}`;
+  pokemonHeight.textContent = `Height: ${height}`;
+  typeNames.map((type) => {
+    return pokemonTypes.innerHTML += `<div class="types-div">${type.toUpperCase()}</div>`;
+  });
+  pokemonImage.innerHTML = `<img id="sprite" src="${image}" alt="Pokemon Image">`;
+  pokemonHp.textContent = `${hp}`;
+  pokemonAttack.textContent = `${attack}`;
+  pokemonDefense.textContent = `${defense}`;
+  pokemonSpAttack.textContent = `${specialAttack}`;
+  pokemonSpDefense.textContent = `${specialDefense}`;
+  pokemonSpeed.textContent = `${speed}`;
 
 }
 
 searchButton.addEventListener('click', () => {
+
+  const detailsElements = document.querySelectorAll(".details");
+  detailsElements.forEach(elem => elem.innerHTML = "" );
+  // pokemonName.textContent = ``;
+  // pokemonId.textContent = ``;
+  // pokemonWeight.textContent = ``;
+  // pokemonHeight.textContent = ``;
+  // pokemonTypes.innerHTML = ``
+  // pokemonImage.innerHTML = ``;
+  // pokemonHp.textContent = ``;
+  // pokemonAttack.textContent = ``;
+  // pokemonDefense.textContent = ``;
+  // pokemonSpAttack.textContent = ``;
+  // pokemonSpDefense.textContent = ``;
+  // pokemonSpeed.textContent = ``;
   
   if(searchInput.value) {
     fetchData();
